258af2689266a91e954708a3e982edc71bce1f41
James <snazzyjames@gmail.com>
2014-08-07 23:44:11 -0500

    login form and validators

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -6,6 +6,31 @@
  */
 
 module.exports = {
-	
+
+    'new':function(req,res) {
+        res.locals.flash = _.clone(req.session.flash);
+        res.view();
+        req.session.flash = {};
+    },
+
+    create: function(req, res, next) {
+        User.create( req.params.all(), function userCreated(err, user) {
+
+            //If there's an error
+            //if(err) return next(err);
+            if(err){
+                console.log(err);
+                req.session.flash = {
+                    err: err
+                }
+
+                return res.redirect('/user/new');
+            }
+
+            //After successfully creating the user, redirect to the show action
+            res.json(user);
+            req.session.flash = {};
+        })
+    }
 };
 
--- api/models/User.js
+++ api/models/User.js
@@ -13,14 +13,34 @@ module.exports = {
             primaryKey: true,
             unique: true
         },
-        email: { type: 'string' },
+        email: {
+            type: 'string',
+            required: true,
+            email: true,
+            unique: true
+        },
         password: { type: 'string' },
-        keyId: { type: 'string' },
-        vCode: { type: 'string' }
+        keyId: {
+            type: 'string',
+            unique: true
+        },
+        vCode: {
+            type: 'string',
+            unique: true
+        }
     },
 
     beforeCreate: function(attrs, next) {
 
+    },
+
+    toJSON: function() {
+        var obj = this.toObject();
+        delete obj.passsword;
+        delete obj.confirmation;
+        delete obj.encryptedPassword;
+        delete obj._csrf;
+        return obj;
     }
 };
 
